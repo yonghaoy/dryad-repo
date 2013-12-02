6c398ada8dfb028c51f465dc6d8187d7ea6e70c9
Dan Leehr <dan.leehr@nescent.org>
2013-12-02 11:14:23 -0500

    Revert "atmire-1691"
    atmire-1691 broke journal config parsing
    
    This reverts commit 827af23da356397437815b8b72e3f9319981e29a.

--- dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/SelectPublicationStep.java
+++ dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/submission/submit/SelectPublicationStep.java
@@ -132,10 +132,6 @@ public class SelectPublicationStep extends AbstractSubmissionStep {
 
         // retrieve request parameters: journlaID, manuscriptNumber
         String selectedJournalId = request.getParameter("journalID");
-        if(selectedJournalId!=null)
-        {
-            selectedJournalId = selectedJournalId.toLowerCase();
-        }
         String manuscriptNumber = request.getParameter("manu");
 	log.debug("initializing submission UI for journal " + selectedJournalId + ", manu " + manuscriptNumber);
         PublicationBean pBean = null;
@@ -375,10 +371,6 @@ public class SelectPublicationStep extends AbstractSubmissionStep {
                 }
                 else if(request.getParameter("journalIDStatusInReview")!=null&&!request.getParameter("journalIDStatusInReview").equals("")){
                     selectedJournalId = request.getParameter("journalIDStatusInReview");
-                    if(selectedJournalId!=null)
-                    {
-                        selectedJournalId = selectedJournalId.toLowerCase();
-                    }
                     journalID.addOption(val.equals(selectedJournalId), val, name);
                 }
                 else{
